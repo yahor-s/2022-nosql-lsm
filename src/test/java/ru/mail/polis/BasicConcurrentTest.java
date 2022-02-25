83e2da374106d19872fbe77f6ffc42b3c84c8d0f
Artyom Drozdov <artem.drozdov@corp.mail.ru>
2022-02-26 01:15:29 +0300

    Relaxed RW-test

--- src/test/java/ru/mail/polis/BasicConcurrentTest.java
+++ src/test/java/ru/mail/polis/BasicConcurrentTest.java
@@ -18,8 +18,8 @@ public class BasicConcurrentTest extends BaseTest {
     }
 
     @DaoTest
-    void testConcurrentRW_10_000(Dao<String, Entry<String>> dao) throws Exception {
-        int count = 10_000;
+    void testConcurrentRW_5_000(Dao<String, Entry<String>> dao) throws Exception {
+        int count = 5_000;
         List<Entry<String>> entries = entries("k", "v", count);
         runInParallel(100, count, value -> {
             dao.upsert(entries.get(value));
