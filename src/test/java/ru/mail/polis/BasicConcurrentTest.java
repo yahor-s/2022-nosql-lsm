973c11ae76fc0b019ffd2ade18d05151c5bdb76c
Artyom Drozdov <artem.drozdov@corp.mail.ru>
2022-02-27 12:49:04 +0300

    Tuning RW_2_500 test

--- src/test/java/ru/mail/polis/BasicConcurrentTest.java
+++ src/test/java/ru/mail/polis/BasicConcurrentTest.java
@@ -18,8 +18,8 @@ public class BasicConcurrentTest extends BaseTest {
     }
 
     @DaoTest
-    void testConcurrentRW_5_000(Dao<String, Entry<String>> dao) throws Exception {
-        int count = 5_000;
+    void testConcurrentRW_2_500(Dao<String, Entry<String>> dao) throws Exception {
+        int count = 2_500;
         List<Entry<String>> entries = entries("k", "v", count);
         runInParallel(100, count, value -> {
             dao.upsert(entries.get(value));
