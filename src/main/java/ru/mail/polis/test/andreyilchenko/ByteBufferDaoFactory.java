df5fa68cf857959ba06c46d5ba74736e80271b35
sorgeligt <52414258+sorgeligt@users.noreply.github.com>
2022-03-03 20:13:57 +0300

    Stage 1 - Ilchenko Andrey (#15)
    * first try InMemoryDao
    
    * I'm not sure, but I think it's better to do it this way
    
    * Time fix & refactoring
    
    * another fix InMemoryDao
    
    Co-authored-by: Artyom Drozdov <artyomcool2@gmail.com>

---
+++ src/main/java/ru/mail/polis/test/andreyilchenko/ByteBufferDaoFactory.java
@@ -0,0 +1,38 @@
+package ru.mail.polis.test.andreyilchenko;
+
+import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Dao;
+import ru.mail.polis.Entry;
+import ru.mail.polis.andreyilchenko.InMemoryDao;
+import ru.mail.polis.test.DaoFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+@DaoFactory
+public class ByteBufferDaoFactory implements DaoFactory.Factory<ByteBuffer, BaseEntry<ByteBuffer>> {
+
+    @Override
+    public Dao<ByteBuffer, BaseEntry<ByteBuffer>> createDao() {
+        return new InMemoryDao();
+    }
+
+    @Override
+    public String toString(ByteBuffer data) {
+        if (data == null) {
+            return null;
+        }
+        int dataCurrentPosition = data.arrayOffset() + data.position();
+        return new String(data.array(), dataCurrentPosition, data.remaining(), StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public ByteBuffer fromString(String data) {
+        return data == null ? null : ByteBuffer.wrap(data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public BaseEntry<ByteBuffer> fromBaseEntry(Entry<ByteBuffer> baseEntry) {
+        return new BaseEntry<>(baseEntry.key(), baseEntry.value());
+    }
+}
