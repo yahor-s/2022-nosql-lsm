a4d1bbf342108772df456d3a103db2f914a83235
Pobedonostsev Kirill <58333248+Pobedos@users.noreply.github.com>
2022-03-11 16:30:29 +0300

    Fix test timeout (#63)

--- src/test/java/ru/mail/polis/DaoTest.java
+++ src/test/java/ru/mail/polis/DaoTest.java
@@ -36,7 +36,7 @@ import java.util.stream.Stream;
 @ParameterizedTest
 @ArgumentsSource(DaoTest.DaoList.class)
 @ExtendWith(DaoTest.DaoList.class)
-@Timeout(5000)
+@Timeout(5)
 public @interface DaoTest {
 
     int stage();
