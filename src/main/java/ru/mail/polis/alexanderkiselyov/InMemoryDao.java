0bee9940cda4be3eab50a53fa5c1fd92945540cb
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-03-18 19:42:05 +0300

    Kiselyov Alexander, Stage 2 (#37)
    * GradleFix
    
    * GradleFix
    
    * GradleFix
    
    * GradleFix
    
    * Codeclimate fix
    
    * Stage fix
    
    * Buffer fix

--- src/main/java/ru/mail/polis/alexanderkiselyov/InMemoryDao.java
+++ src/main/java/ru/mail/polis/alexanderkiselyov/InMemoryDao.java
@@ -1,17 +1,40 @@
 package ru.mail.polis.alexanderkiselyov;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Stream;
 
 public class InMemoryDao implements Dao<byte[], BaseEntry<byte[]>> {
-    Comparator<byte[]> byteArrayComparator = Arrays::compare;
-    NavigableMap<byte[], BaseEntry<byte[]>> pairs = new ConcurrentSkipListMap<>(byteArrayComparator);
+    private final NavigableMap<byte[], BaseEntry<byte[]>> pairs;
+    private final Config config;
+    private final int bufferSize = 1000 * Character.BYTES;
+    private static final String FILE_NAME = "myData";
+    private long filesCount;
+
+    public InMemoryDao(Config config) throws IOException {
+        this.config = config;
+        pairs = new ConcurrentSkipListMap<>(Arrays::compare);
+        if (Files.exists(config.basePath())) {
+            try (Stream<Path> stream = Files.list(config.basePath())) {
+                filesCount = stream.count();
+            }
+        } else {
+            filesCount = 0;
+        }
+    }
 
     @Override
     public Iterator<BaseEntry<byte[]>> get(byte[] from, byte[] to) {
@@ -26,7 +49,55 @@ public class InMemoryDao implements Dao<byte[], BaseEntry<byte[]>> {
     }
 
     @Override
+    public BaseEntry<byte[]> get(byte[] key) throws IOException {
+        BaseEntry<byte[]> value = pairs.get(key);
+        if (value != null && Arrays.equals(value.key(), key)) {
+            return value;
+        }
+        return findInFiles(key);
+    }
+
+    @Override
     public void upsert(BaseEntry<byte[]> entry) {
         pairs.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        Path newFilePath = config.basePath().resolve(FILE_NAME + filesCount + ".txt");
+        if (!Files.exists(newFilePath)) {
+            Files.createFile(newFilePath);
+        }
+        try (FileOutputStream fos = new FileOutputStream(String.valueOf(newFilePath));
+             BufferedOutputStream writer = new BufferedOutputStream(fos, bufferSize)) {
+            for (var pair : pairs.entrySet()) {
+                writer.write(pair.getKey().length);
+                writer.write(pair.getKey());
+                writer.write(pair.getValue().value().length);
+                writer.write(pair.getValue().value());
+            }
+        }
+        filesCount++;
+    }
+
+    private BaseEntry<byte[]> findInFiles(byte[] key) throws IOException {
+        for (long i = filesCount - 1; i >= 0; i--) {
+            Path currentFile = config.basePath().resolve(FILE_NAME + i + ".txt");
+            try (FileInputStream fis = new FileInputStream(String.valueOf(currentFile));
+                 BufferedInputStream reader = new BufferedInputStream(fis, bufferSize)) {
+                while (reader.available() != 0) {
+                    int keyLength = reader.read();
+                    byte[] currentKey = reader.readNBytes(keyLength);
+                    int valueLength = reader.read();
+                    byte[] currentValue = reader.readNBytes(valueLength);
+                    if (Arrays.equals(currentKey, key)) {
+                        reader.close();
+                        fis.close();
+                        return new BaseEntry<>(currentKey, currentValue);
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
--- src/main/java/ru/mail/polis/test/alexanderkiselyov/ByteArrayDaoFactory.java
+++ src/main/java/ru/mail/polis/test/alexanderkiselyov/ByteArrayDaoFactory.java
@@ -1,18 +1,20 @@
 package ru.mail.polis.test.alexanderkiselyov;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.alexanderkiselyov.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@DaoFactory
+@DaoFactory(stage = 2, week = 3)
 public class ByteArrayDaoFactory implements DaoFactory.Factory<byte[], BaseEntry<byte[]>> {
     @Override
-    public Dao<byte[], BaseEntry<byte[]>> createDao() {
-        return new InMemoryDao();
+    public Dao<byte[], BaseEntry<byte[]>> createDao(Config config) throws IOException {
+        return new InMemoryDao(config);
     }
 
     @Override
