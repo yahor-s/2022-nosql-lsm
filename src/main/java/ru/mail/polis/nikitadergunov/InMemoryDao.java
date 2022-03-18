836fbf50439eaba627ca1f8bf37e270717c2ef43
Nikita Dergunov <62371194+nikita17th@users.noreply.github.com>
2022-03-18 22:08:51 +0300

    Stage 2 Dergunov Nikita (#59)
    * STAGE-2 Add implementation
    
    * STAGE-2 Fix factory
    
    * STAGE-2 Fix factory
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Fix comparator, allocate ByteBuffer
    
    * STAGE-2 Fix exceptions
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Remove syscall
    
    * STAGE-2 Remove syscall
    
    * STAGE-2 Add scope
    
    * STAGE-2 Fix write and read
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Fix read and locks
    
    * STAGE-2 Fix cg
    
    * STAGE-2 Micro fix
    
    * STAGE-2 Micro fix
    
    Co-authored-by: n.dergunov <n.dergunov@vk.team>

--- src/main/java/ru/mail/polis/nikitadergunov/InMemoryDao.java
+++ src/main/java/ru/mail/polis/nikitadergunov/InMemoryDao.java
@@ -2,47 +2,106 @@ package ru.mail.polis.nikitadergunov;
 
 import jdk.incubator.foreign.MemoryAccess;
 import jdk.incubator.foreign.MemorySegment;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> storage =
-            new ConcurrentSkipListMap<>((MemorySegment firstSegment, MemorySegment secondSegment) -> {
-                long offsetMismatch = firstSegment.mismatch(secondSegment);
-                if (offsetMismatch == -1) {
-                    return 0;
-                }
-                return Byte.compare(MemoryAccess.getByteAtOffset(firstSegment, offsetMismatch),
-                        MemoryAccess.getByteAtOffset(secondSegment, offsetMismatch));
-            }
-            );
+            new ConcurrentSkipListMap<>(InMemoryDao::comparator);
+    private final Config config;
+    private final ReadFromNonVolatileMemory readFromNonVolatileMemory;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final AtomicLong sizeInBytes = new AtomicLong(0);
+
+    public static int comparator(MemorySegment firstSegment, MemorySegment secondSegment) {
+        long offsetMismatch = firstSegment.mismatch(secondSegment);
+        if (offsetMismatch == -1) {
+            return 0;
+        }
+        if (offsetMismatch == firstSegment.byteSize()) {
+            return -1;
+        }
+        if (offsetMismatch == secondSegment.byteSize()) {
+            return 1;
+        }
+        return Byte.compare(MemoryAccess.getByteAtOffset(firstSegment, offsetMismatch),
+                MemoryAccess.getByteAtOffset(secondSegment, offsetMismatch));
+    }
+
+    public InMemoryDao(Config config) throws IOException {
+        this.config = config;
+        readFromNonVolatileMemory = new ReadFromNonVolatileMemory(config);
+    }
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        return storage.get(key);
+        lock.readLock().lock();
+        try {
+            Entry<MemorySegment> value = storage.get(key);
+            if (value == null && readFromNonVolatileMemory.isExist()) {
+                value = readFromNonVolatileMemory.get(key);
+            }
+            return value;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        if (from == null && to == null) {
-            return storage.values().iterator();
+        lock.readLock().lock();
+        try {
+            if (from == null && to == null) {
+                return storage.values().iterator();
+            }
+            if (from == null) {
+                return storage.headMap(to).values().iterator();
+            }
+            if (to == null) {
+                return storage.tailMap(from).values().iterator();
+            }
+            return storage.subMap(from, to).values().iterator();
+        } finally {
+            lock.readLock().unlock();
         }
-        if (from == null) {
-            return storage.headMap(to).values().iterator();
-        }
-        if (to == null) {
-            return storage.tailMap(from).values().iterator();
-        }
-        return storage.subMap(from, to).values().iterator();
+
     }
 
     @Override
     public void upsert(Entry<MemorySegment> entry) {
-        storage.put(entry.key(), entry);
+        lock.readLock().lock();
+        try {
+            storage.put(entry.key(), entry);
+            sizeInBytes.addAndGet(entry.key().byteSize());
+            if (entry.value() != null) {
+                sizeInBytes.addAndGet(entry.value().byteSize());
+            }
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
+    @Override
+    public void close() throws IOException {
+        readFromNonVolatileMemory.close();
+        lock.writeLock().lock();
+        try {
+            WriteToNonVolatileMemory writeToNonVolatileMemory =
+                    new WriteToNonVolatileMemory(config, storage, sizeInBytes.get());
+            writeToNonVolatileMemory.write();
+            writeToNonVolatileMemory.close();
+        } finally {
+            lock.writeLock().lock();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/test/nikitadergunov/MemorySegmentDaoFactory.java
+++ src/main/java/ru/mail/polis/test/nikitadergunov/MemorySegmentDaoFactory.java
@@ -2,28 +2,32 @@ package ru.mail.polis.test.nikitadergunov;
 
 import jdk.incubator.foreign.MemorySegment;
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.nikitadergunov.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
 
-@DaoFactory(stage = 1, week = 2)
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@DaoFactory(stage = 2, week = 2)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
 
     @Override
-    public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
-        return new InMemoryDao();
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
+        return new InMemoryDao(config);
     }
 
     @Override
     public String toString(MemorySegment data) {
-        return data == null ? null : new String(data.toCharArray());
+        return data == null ? null : new String(data.toByteArray(), StandardCharsets.UTF_8);
 
     }
 
     @Override
     public MemorySegment fromString(String data) {
-        return data == null ? null : MemorySegment.ofArray(data.toCharArray());
+        return data == null ? null : MemorySegment.ofArray(data.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
