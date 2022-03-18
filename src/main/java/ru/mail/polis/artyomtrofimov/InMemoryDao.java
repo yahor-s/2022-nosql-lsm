be74075f032b376778d1a166bdd5bf7f21225f54
Artyom Trofimov <troart@bk.ru>
2022-03-18 16:21:53 +0300

    Trofimov Artyom, stage 2 (#41)
    * наработки для stage2
    
    * implement stage2 [WIP]
    
    * codestyle fix
    
    * performance fix
    
    * fix codestyle
    
    * fix codestyle
    
    * refactoring
    
    * small fix
    
    * refactoring
    
    * change to week 2
    
    * return to week 1
    
    * поправлены указанные недочеты
    
    * refactoring
    
    Co-authored-by: Artyom Trofimov <artem24630@gmail.com>

--- src/main/java/ru/mail/polis/artyomtrofimov/InMemoryDao.java
+++ src/main/java/ru/mail/polis/artyomtrofimov/InMemoryDao.java
@@ -1,15 +1,34 @@
 package ru.mail.polis.artyomtrofimov;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
+import ru.mail.polis.Entry;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
-public class InMemoryDao implements Dao<String, BaseEntry<String>> {
-    ConcurrentSkipListMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
+public class InMemoryDao implements Dao<String, Entry<String>> {
+    private static final String FILENAME = "db.txt";
+    private final ConcurrentNavigableMap<String, Entry<String>> data = new ConcurrentSkipListMap<>();
+    private final Path dataPath;
+    private long lastWritePos;
+    private volatile boolean commit;
+
+    public InMemoryDao(Config config) {
+        if (config == null) {
+            throw new IllegalArgumentException("Config shouldn't be null");
+        }
+        dataPath = config.basePath().resolve(FILENAME);
+    }
 
     @Override
-    public Iterator<BaseEntry<String>> get(String from, String to) {
+    public Iterator<Entry<String>> get(String from, String to) throws IOException {
         boolean isFromNull = from == null;
         boolean isToNull = to == null;
         if (isFromNull && isToNull) {
@@ -25,7 +44,61 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     }
 
     @Override
-    public void upsert(BaseEntry<String> entry) {
+    public Entry<String> get(String key) throws IOException {
+        Entry<String> entry = data.get(key);
+        if (entry == null) {
+            entry = findInFileByKey(key);
+            if (entry != null) {
+                data.put(entry.key(), entry);
+            }
+            return entry;
+        }
+        return entry;
+    }
+
+    private Entry<String> findInFileByKey(String key) throws IOException {
+        try (RandomAccessFile input = new RandomAccessFile(dataPath.toString(), "r")) {
+            input.seek(0);
+            String line;
+            while (input.getFilePointer() <= input.length()) {
+                line = input.readUTF();
+                int delimiterIndex = line.indexOf(' ');
+                if (delimiterIndex == -1) {
+                    continue;
+                }
+                int keyLength = Integer.parseInt(line, 0,delimiterIndex, 10);
+                String currentKey = line.substring(delimiterIndex + 1, delimiterIndex + keyLength + 1);
+                if (key.equals(currentKey)) {
+                    return new BaseEntry<>(currentKey, line.substring(delimiterIndex + keyLength + 1));
+                }
+            }
+            return null;
+        } catch (FileNotFoundException | EOFException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void upsert(Entry<String> entry) {
         data.put(entry.key(), entry);
+        commit = false;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        if (commit) {
+            return;
+        }
+        try (RandomAccessFile output = new RandomAccessFile(dataPath.toString(), "rw")) {
+            output.seek(lastWritePos);
+            StringBuilder result = new StringBuilder();
+            for (Entry<String> value : data.values()) {
+                result.append(value.key().length()).append(' ').append(value.key()).append(value.value());
+                output.writeUTF(result.toString());
+                result.setLength(0);
+            }
+            lastWritePos = output.getFilePointer();
+        }
+        commit = true;
     }
 }
--- src/main/java/ru/mail/polis/test/artyomtrofimov/StringDaoFactory.java
+++ src/main/java/ru/mail/polis/test/artyomtrofimov/StringDaoFactory.java
@@ -1,17 +1,15 @@
 package ru.mail.polis.test.artyomtrofimov;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.artyomtrofimov.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
+import java.io.IOException;
 
-@DaoFactory
-public class StringDaoFactory implements DaoFactory.Factory<String, BaseEntry<String>> {
-    @Override
-    public Dao<String, BaseEntry<String>> createDao() {
-        return new InMemoryDao();
-    }
+@DaoFactory(stage = 2, week = 2)
+public class StringDaoFactory implements DaoFactory.Factory<String, Entry<String>> {
 
     @Override
     public String toString(String data) {
@@ -26,5 +24,10 @@ public class StringDaoFactory implements DaoFactory.Factory<String, BaseEntry<St
     @Override
     public BaseEntry<String> fromBaseEntry(Entry<String> baseEntry) {
         return new BaseEntry<>(baseEntry.key(), baseEntry.value());
+    }
+
+    @Override
+    public Dao<String, Entry<String>> createDao(Config config) throws IOException {
+        return new InMemoryDao(config);
     }
 }
